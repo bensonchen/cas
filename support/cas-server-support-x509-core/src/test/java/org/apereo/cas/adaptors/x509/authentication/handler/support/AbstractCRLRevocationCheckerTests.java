f6ff900c6e3a9d6a8abb42ca036bfa02267dc264
Timur Duehr <tduehr@gmail.com>
2018-12-03 17:50:55 -0600

    fixup! CRL

--- support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/AbstractCRLRevocationCheckerTests.java
+++ support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/AbstractCRLRevocationCheckerTests.java
@@ -4,11 +4,12 @@ import org.apereo.cas.adaptors.x509.authentication.revocation.checker.AbstractCR
 import org.apereo.cas.adaptors.x509.authentication.revocation.checker.RevocationChecker;
 import org.apereo.cas.util.crypto.CertUtils;
 
+import lombok.val;
 import org.springframework.core.io.ClassPathResource;
 
 import java.security.GeneralSecurityException;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.apereo.cas.util.AssertThrows.*;
 
@@ -23,10 +24,16 @@ public abstract class AbstractCRLRevocationCheckerTests {
      * Test method for {@link AbstractCRLRevocationChecker#check(X509Certificate)}.
      */
     public void checkCertificate(final AbstractCRLRevocationChecker checker, final String[] certFiles, final GeneralSecurityException expected) {
-        Arrays.stream(certFiles)
-            .map(file -> CertUtils.readCertificate(new ClassPathResource(file)))
-            .forEach(cert -> {
-                assertThrowsOrNot(expected, () -> checker.check(cert));
-            });
+        val certificates = new X509Certificate[certFiles.length];
+        val i = new AtomicInteger();
+        for (val file : certFiles) {
+            certificates[i.getAndIncrement()] = CertUtils.readCertificate(new ClassPathResource(file));
+        }
+
+        assertThrowsOrNot(expected, () -> {
+            for (val cert : certificates) {
+                checker.check(cert);
+            }
+        });
     }
 }
